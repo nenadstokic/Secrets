91f993520aa177fc2c8ef0deab98f02730139d4b
Nenad <nstokic@gmail.com>
2019-02-24 17:31:50 +0100

    level 6 completed G auth, begin FB auth

--- app.js
+++ app.js
@@ -32,7 +32,8 @@ mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema ({
   email: String,
-  password: String
+  password: String,
+  googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -41,16 +42,25 @@ userSchema.plugin(findOrCreate);
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
 
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/Secrets",
+    callbackURL: "http://localhost:3000/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
       return cb(err, user);
     });
@@ -61,9 +71,16 @@ app.get("/", function(req, res){
   res.render("home");
 });
 
-app.get("/auth/google", function(req, res){
-  passport.authenticate("google", {scope: ["profile"] });
+app.get("/auth/google",
+  passport.authenticate("google", {scope: ["profile"] })
+);
+
+app.get("/auth/google/secrets",
+  passport.authenticate("google", {failureRedirect: "/login" }),
+  function(req, res) {
+    res.redirect("/secrets");
 });
+
 
 app.get("/login", function(req, res){
   res.render("login");
